ba0081f39e8c42fcbe348b3691b2ef9671de0019
Monte Johnston <johnston.monte@gmail.com>
2020-11-08 09:23:41 -0500

    Make it prettier

--- lib/inform_web/live/send_live.ex
+++ lib/inform_web/live/send_live.ex
@@ -55,48 +55,4 @@ defmodule InformWeb.SendLive do
        to: Routes.recipients_path(socket, :index)
      )}
   end
-
-  @impl true
-  def render(assigns) do
-    IO.puts("rendering")
-
-    ~L"""
-      <div class="row">
-    <div  style="display:<%= if @page == 1 do 'block' else 'none' end %>">
-      <section class="phx-hero">
-        <form phx-change="filter" >
-          <label for="region">Region</label>
-          <select name="region" id="region-select">
-            <%= for region <- @regions do %>
-              <option value="<%= region %>" <%= region === @selected_region && "selected " %>><%= region %></option>
-            <% end %>
-          </select>
-          <button  type="button" phx-click="next" phx-value-page="2" >Next</button>
-        </form>
-      </section>
-      <section class="row">
-        <div class="grid">
-          <span class="table-header">Last Name</span>
-          <span class="table-header">First Name</span>
-          <span class="table-header">Region</span>
-          <%= for %{first_name: first_name, last_name: last_name, region: region} <- @recipients do %>
-            <span ><%= last_name %></span>
-            <span ><%= first_name %></span>
-            <span ><%= region %></span>
-          <% end %>
-        </div>
-      </section>
-    </div>
-    <section class="phx-hero" style="display:<%= if @page == 2 do 'block' else 'none' end %>">
-      <p>This message will go to <%= length(@recipients) %> recipients</p>
-      <form  phx-change="message" phx-submit="send">
-      <label for="message">Message</label>
-      <textarea name="message" autofocus /></textarea>
-
-      <button phx-click="back" phx-value-page="1" type="button" >Back</button>
-      <button type="submit" phx-disable-with="Searching...">Send</button>
-      </form>
-    </section>
-    """
-  end
 end
